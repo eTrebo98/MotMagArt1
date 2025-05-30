448e0fc421fa57cb6e67c61bf2b02672a7c92738
Edin <trebovicedin@gmail.com>
2025-05-30 12:00:27 +0200

    Update CompElemMag.m

--- CompElemMag.m
+++ CompElemMag.m
@@ -4,6 +4,14 @@ function [elemMag, elemConj] = CompElemMag(fft1,fft2,alpha)
     %          fft2, the current element of the fft matrix of the first frame
     %   OUTPUT: elemMag, the element of the fft of the magnified frame
     %           elemConj, the conjugate element of elemMag
+    %
+    % Copyright (c) 2024-2025 University of Camerino, 2T System srl
+    %
+    % Authors: Nadaniela Egidi, Josephin Giacomini, Paolo Leonesi, Pierluigi Maponi, Federico Mearelli, Edin Trebovic 
+    %
+    % Date: Dec 2024
+    %
+
     
     %computing the normalized ratio between the current fft elements of the
     %consecutive frame
